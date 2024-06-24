b5865e7522e869785b7f163f33fc10f0b913c3a7
Laptop <laptop@disroot.org>
2024-06-24 18:06:29 +0300

    wip wip wip should work but didnt test

--- src/main.rs
+++ src/main.rs
@@ -8,13 +8,14 @@ use url::Url;
 
 const RULES_FILE_PATH: &str = "./src/rules.json";
 const TYPE_BLACKLIST: u8 = 0;
-const TYPE_WHITELIST: u8 = 0;
+const TYPE_WHITELIST: u8 = 1;
 
 #[derive(Deserialize, Debug)]
 struct Rule {
     domain: Value,
     #[serde(rename = "type")]
     type_field: u8,
+    path: String,
     params: Vec<String>,
 }
 
@@ -43,10 +44,16 @@ fn main() {
         }
     };
 
-    let input_url_host = match &input_url.host() {
-        Some(h) => h.to_string().replace("www.", ""),
+    let mut input_url_host = match &input_url.host() {
+        Some(h) => h.to_string(),
         None => String::from("No host found"),
     };
+
+    if input_url_host.starts_with("www.") {
+        input_url_host = input_url_host[4..].to_string();
+    }
+
+    let input_url_path = input_url.path();
 
     let mut stripped_url = Url::parse(&input_url.to_string()).unwrap();
 
@@ -63,6 +70,10 @@ fn main() {
         let rule_domain = rule.domain.to_string().replace("\"", "");
 
         if input_url_host == rule_domain {
+            if rule.path != "*" && rule.path != input_url_path {
+                continue;
+            }
+
             println!("{} {}", "Detected URL:".green(), rule_domain);
 
             for (key, val) in input_url.query_pairs() {
@@ -90,7 +101,7 @@ fn main() {
             break;
         } else if rule.domain == true {
             for (key, val) in input_url.query_pairs() {
-                if rule.type_field == 0 {
+                if rule.type_field == TYPE_BLACKLIST {
                     if !rule.params.contains(&key.to_string())
                         && !applied_params.contains(&key.to_string())
                     {
@@ -99,7 +110,7 @@ fn main() {
                             .append_pair(&key.to_string(), &val.to_string());
                         applied_params.insert(key.to_string());
                     }
-                } else if rule.type_field == 1 {
+                } else if rule.type_field == TYPE_WHITELIST {
                     if rule.params.contains(&key.to_string())
                         && !applied_params.contains(&key.to_string())
                     {
